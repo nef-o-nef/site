c939807fc2ccf2190877d2c9ce7588441896d343
nefo zhang <nefo@nefodeMacBook-Pro.local>
2026-08-19 19:30:29 +0200

    Add spacetime index

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,5 @@ export default defineConfig({
   // Using nefo.cc as a custom domain on GitHub Pages -> keep base as '/'.
   // Deploying to USER.github.io/REPO instead? set base: '/REPO'.
   site: 'https://www.nefo.cc',
-  base: '/',
+  base: '/site',
 });
